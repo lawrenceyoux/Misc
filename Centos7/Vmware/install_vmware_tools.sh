cc5a36313c08625dd9da23ffd47becdf0ba61d18
lawrenceyoux <lawrenceyoux@gmail.com>
2018-03-12 18:45:57 +0000

    Create install_vmware_tools.sh

---
+++ Centos7/Vmware/install_vmware_tools.sh
@@ -0,0 +1,8 @@
+mkdir /mnt/cdrom
+mount /dev/cdrom /mnt/cdrom
+cp /mnt/cdrom/VMwareTools-10.0.6-3560309.tar.gz /tmp/
+ls /mnt/cdrom
+cd /tmp
+tar -zxvf VMwareTools-10.0.6-3560309.tar.gz
+cd vmware-tools-distrib
+./vmware-install.pl
